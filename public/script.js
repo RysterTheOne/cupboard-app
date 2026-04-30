c38dbdf7351d7782dcc82939d8f775f112474d3f
RysterTheOne <rystergaming1234@gmail.com>
2026-04-30 20:24:57 +0200

    style changes

---
+++ public/script.js
@@ -0,0 +1,28 @@
+const API = "https://cupboard-app.onrender.com";
+
+// Auth Functions //
+async function checkAuth() {
+    try {
+        const res = await fetch(API + "/api/me", {
+            credentials: "include"
+        });
+
+        const data = await res.json();
+
+        if (!data || !data.userId) {
+            window.location.href = "login.html";
+        }
+    } catch (err) {
+        window.location.href = "login.html";
+    }
+}
+checkAuth();
+
+async function logout() {
+    await fetch(API + "/api/logout", {
+        method: "POST",
+        credentials: "include"
+    });
+
+    window.location.href = "login.html";
+}
