afdfaa76c1fab75c961cc25f067fcfc1203f5016
RysterTheOne <rystergaming1234@gmail.com>
2026-05-14 11:02:03 +0200

    script changes

--- server/server.js
+++ server/server.js
@@ -215,7 +215,8 @@ app.post("/api/login", async (req, res) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "None"
+        sameSite: "None",
+        maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
     res.json({ success: true });
